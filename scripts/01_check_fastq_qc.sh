6f40364796bb5eacfa5d2c0f8d9e9281a3158765
chingyaousf <96981462+chingyaousf@users.noreply.github.com>
2023-08-11 17:02:31 -0400

    Create 01_check_fastq_qc.sh

---
+++ scripts/01_check_fastq_qc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --chdir=../Work
+#SBATCH --mail-type=ALL
+#SBATCH --time=00:10:00
+#SBATCH --mem=10000
+#SBATCH --nodes=1
+#SBATCH --mail-user=
+#SBATCH --job-name=FASTQC
+#SBATCH --output=fastqc.out
+
+module load apps/fastqc/0.11.5
+fastqc ../FASTQs/*.fastq.gz --outdir ../FASTQ_QC
