f33e630cca5694ac1ec86de2f6d6085f9b7c0bb0
chingyaousf <96981462+chingyaousf@users.noreply.github.com>
2023-08-11 17:32:37 -0400

    Update 04_run_cufflinks.sh

--- scripts/04_run_cufflinks.sh
+++ scripts/04_run_cufflinks.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=00:30:00
 #SBATCH --mem=10000
 #SBATCH --nodes=1
-#SBATCH --mail-user=
+#SBATCH --mail-user=your_email
 #SBATCH --job-name=run_cufflinks
 #SBATCH --output=run_cufflinks.out
 
