7fa61b34a6426da65db068946fda424cf443c800
chingyaousf <96981462+chingyaousf@users.noreply.github.com>
2023-09-08 18:53:13 -0400

    Update 03_run_hisat.sh

--- scripts/03_run_hisat.sh
+++ scripts/03_run_hisat.sh
@@ -44,8 +44,37 @@ SAMPLE4_R2=../FASTQs/drug_rep2_R2.fastq.gz
 SAMPLE4_SAM_OUTFILE=drug_rep2.sam
 SAMPLE4_BAM_OUTFILE=drug_rep2.bam
 
-hisat2 -p $NUMBER_OF_PROCESSORS --dta-cufflinks -x $REF_GENOME  --rna-strandness $RNA_STRANDNESS -1 $SAMPLE1_R1 -2 $SAMPLE1_R2 -S $SAMPLE1_SAM_OUTFILE
+##--dta-cufflinks is an option used to make the output compatible with cufflinks
+
+##The hisat2 command aligns your reads to the reference genome
+##--fr tell hisat2 how your reads are related to each other (Read one is the forward read and read 2 is the reverse of the forward read
+##-p is the number of processors you will be using to compute the alignments
+##--dta-cufflinks is an option used to make the output compatible with cufflinks
+##-x specifies the indexed reference genome that the reads will be aligned to
+##--rna-strandness specifies how the reads related to the original transcript and hence how they relate to your reference genome
+##--1 specifies your read 1 input 
+##--2 specifies your read 2 input
+##-S specifies the out file. The output is a SAM (Sequence alignment map)  file. A SAM file is a text file that  describes how reads align to a reference
+
+##The samtools sort command will sort the alignments by position and convert the file into a binary formant (bam)
+##Pretty much everything you would use an alignment file for requires the alignments to be sorted and bam files take up less space
+##The -@ specifies how many processors to use. 
+##The -o specifies the output file (bam file)
+##The last input is the input sam file
+
+hisat2 --fr -p $NUMBER_OF_PROCESSORS --dta-cufflinks -x $REF_GENOME  --rna-strandness $RNA_STRANDNESS -1 $SAMPLE1_R1 -2 $SAMPLE1_R2 -S $SAMPLE1_SAM_OUTFILE
 samtools sort -@ $NUMBER_OF_PROCESSORS -o $SAMPLE1_BAM_OUTFILE $SAMPLE1_SAM_OUTFILE
 
-#hisat2 -p NUMBER_OF_PROCESSORS --data-cufflinks -x $REF_GENOME --rna-strandness $RNA_STRANDNESS -1 $SAMPLE1_R1 -2 $SAM
-#PLE1_R2 -S $SAMPLE1_SAM_OUTFILE
+hisat2 --fr -p $NUMBER_OF_PROCESSORS --dta-cufflinks -x $REF_GENOME  --rna-strandness $RNA_STRANDNESS -1 $SAMPLE2_R1 -2 $SAMPLE2_R2 -S $SAMPLE2_SAM_OUTFILE
+samtools sort -@ $NUMBER_OF_PROCESSORS -o $SAMPLE2_BAM_OUTFILE $SAMPLE2_SAM_OUTFILE
+
+hisat2 --fr -p $NUMBER_OF_PROCESSORS --dta-cufflinks -x $REF_GENOME  --rna-strandness $RNA_STRANDNESS -1 $SAMPLE3_R1 -2 $SAMPLE3_R2 -S $SAMPLE3_SAM_OUTFILE
+samtools sort -@ $NUMBER_OF_PROCESSORS -o $SAMPLE3_BAM_OUTFILE $SAMPLE3_SAM_OUTFILE
+
+hisat2 --fr -p $NUMBER_OF_PROCESSORS --dta-cufflinks -x $REF_GENOME  --rna-strandness $RNA_STRANDNESS -1 $SAMPLE4_R1 -2 $SAMPLE4_R2 -S $SAMPLE4_SAM_OUTFILE
+samtools sort -@ $NUMBER_OF_PROCESSORS -o $SAMPLE4_BAM_OUTFILE $SAMPLE4_SAM_OUTFILE
+
+
+##Sam files take up a lot of memory. Once you have succesfully sorted your alignments and converted to bam
+##you can run the command below to delete the sam files (make sure you run this in the correct directory
+#rm *.sam
